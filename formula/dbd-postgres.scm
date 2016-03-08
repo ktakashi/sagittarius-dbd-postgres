2de1fd9f695da69ec52000b0182d7d3ebbf90b15
ktakashi <ktakashi@ymail.com>
2016-03-08 12:49:39 +0100

    Adding formula

---
+++ formula/dbd-postgres.scm
@@ -0,0 +1,13 @@
+(formula
+ (description "PostgreSQL DBD for Sagittarius")
+ (version "HEAD")
+ (homepage :url "https://github.com/ktakashi/sagittarius-dbd-postgres")
+ (dependencies (dependency :name "postgresql" :version "16.03.08"))
+ (author :name "Takashi Kato" :email "ktakashi@ymail.com")
+ (source 
+  ;;:type tar :compression gzip
+  :type zip
+  :url "https://github.com/ktakashi/sagittarius-dbd-postgres/archive/master.zip")
+
+ (install 
+  (files "dbd/postgres.scm")))
